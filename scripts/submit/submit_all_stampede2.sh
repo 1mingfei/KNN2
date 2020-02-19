09b68e2956a6a4016d20e169defaead782f6f00f
1mingfei <mingfei@umich.edu>
2020-02-19 10:48:50 -0500

    more scripts

--- scripts/submit/submit_all_stampede2.sh
+++ scripts/submit/submit_all_stampede2.sh
@@ -1,11 +1,11 @@
 for i in `seq 0 62`
 do
-    cd config$i
+    cd config$i;
     for j in $(ls -d */)
     do
-        cd config${i}/${j}
+        cd ${j}
         sbatch submit.stampede2
-        cd ../..
+        cd ..
     done
     cd ..
 done
