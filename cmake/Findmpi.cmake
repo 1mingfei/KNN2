6d84d94113ff123c185c79f4d6bc028e82802364
zhucongx <zhucongx@umich.edu>
2020-02-29 18:10:55 -0500

    cmake files update

--- cmake/Findmpi.cmake
+++ cmake/Findmpi.cmake
@@ -1,19 +1,18 @@
 if (APPLE)
     message("Looking for mpi on macOS")
-    set(FIND_ARMADILLO_PATHS /usr/local/)
+    set(FIND_MPI_PATHS /usr/local/)
 elseif (UNIX)
     message("Looking for mpi on Linux")
-    set(FIND_ARMADILLO_PATHS /usr/)
+    set(FIND_MPI_PATHS /usr/lib/x86_64-linux-gnu/openmpi/)
 endif()
 
-find_path(LIBMPI_INCLUDE_DIR mpi.h
-        PATH_SUFFIXES include
-        PATHS ${FIND_MPI_PATHS})
+find_path(LIBMPI_INCLUDE_DIR
+        NAME mpi.h
+        PATHS ${FIND_MPI_PATHS}/include)
 
 find_library(LIBMPI_LIBRARY
         NAMES mpi
-        PATH_SUFFIXES lib
-        PATHS ${FIND_MPI_PATHS})
+        PATHS ${FIND_MPI_PATHS}/lib)
 
 include_directories(${LIBMPI_INCLUDE_DIR})
 target_link_libraries(${PROJECT_NAME} ${LIBMPI_LIBRARY})
