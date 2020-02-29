aec381b4d51f33d72b8702b594652abb506e49f1
zhucongx <xizhucong@gmail.com>
2020-02-29 02:22:06 -0500

    support linux

--- cmake/Findarmadillo.cmake
+++ cmake/Findarmadillo.cmake
@@ -1,4 +1,10 @@
-set(FIND_ARMADILLO_PATHS /usr/local/)
+if (APPLE)
+    message("Working on macOS")
+    set(FIND_ARMADILLO_PATHS /usr/local/)
+elseif (UNIX)
+    message("Working on Linux")
+    set(FIND_ARMADILLO_PATHS /usr/)
+endif()
 
 find_path(LIBARMADILLO_INCLUDE_DIR armadillo
         PATH_SUFFIXES include
--- cmake/Findmpi.cmake
+++ cmake/Findmpi.cmake
@@ -1,4 +1,10 @@
-set(FIND_MPI_PATHS /usr/local)
+if (APPLE)
+    message("Working on macOS")
+    set(FIND_ARMADILLO_PATHS /usr/local/)
+elseif (UNIX)
+    message("Working on Linux")
+    set(FIND_ARMADILLO_PATHS /usr/)
+endif()
 
 find_path(LIBMPI_INCLUDE_DIR mpi.h
         PATH_SUFFIXES include
