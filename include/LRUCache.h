e918948436dba06c6d8f0eac58abd68e48202e47
Zhucong Xi <43595065+zhucongx@users.noreply.github.com>
2020-03-14 22:51:20 -0400

    Update LRUCache.h

--- include/LRUCache.h
+++ include/LRUCache.h
@@ -1,67 +1,173 @@
-#ifndef _LRU_CACHE_H_
-#define _LRU_CACHE_H_
-// modified from geeksforgeeks.com, originally contributed by Satish Srinivas
+#ifndef _GB_CNF_H_
+#define _GB_CNF_H_
 
-
-// We can use stl container list as a double
-// ended queue to store the cache keys, with
-// the descending time of reference from front
-// to back and a set container to check presence
-// of a key. But to fetch the address of the key
-// in the list using find(), it takes O(N) time.
-// This can be optimized by storing a reference
-//   (iterator) to each key in a hash map.
-
+#include "armadillo"
 #include "KNHome.h"
-#define NB 28
-#define KEY_SIZE 27
-class gbCnf;
+#include "LRUCache.h"
+#include "OrderedStruct.h"
+#include <math.h>
 
-namespace std
-{
-template<typename T>
-struct hash<vector<T> > {
-  typedef vector<T> argument_type;
-  typedef size_t result_type;
-  size_t N = KEY_SIZE;
+using arma::mat;
+using arma::vec;
+using std::vector;
+using std::pair;
+using std::map;
+using std::unordered_multimap;
 
-  result_type operator()(const argument_type& a) const {
-    result_type h = a[0];
-    for (result_type i = 1; i < N; ++i) {
-      h <<= 2;
-      h += a[i];
-    }
-    return h;
-  }
-};
-}
+using keras2cpp::Model;
+using keras2cpp::Tensor;
 
-// namespace LR {
+class LRUCache;
 
-using std::list;
-
-class LRUCache {
-  long long ct;
-  // store keys of cache
-  list<pair<string, double>> dq;
-
-  // store references of key in cache
-  unordered_map<string, list<pair<string, double>>::iterator> m;
-  int cSize; // maximum capacity of cache
+class gbCnf {
 
 public:
-  LRUCache();
-  LRUCache(const int&);
-  void setSize(const int&);
-  void add(const pair<vector<int>, double>&);
-  void add(const pair<string, double>&);
-  bool check(const vector<int>&) const;
-  bool check(const string&) const;
-  double getBarrier(const vector<int>&);
-  double getBarrier(const string&);
-  int getSize() const;
-  void display();
-  long long getCt() const;
+  int& me;
+  int& nProcs;
+
+  double rcut;
+  gbCnf(int&, int&);
+
+  /* gbInCnf.cpp */
+  Config readLmpData(const string&);
+  Config readCfg(const string&);
+  Config readPOSCAR(const string&);
+
+  /* output
+   * gbOutCnf.cpp */
+  double findMass(string);
+  void writeLmpData(Config&, string);
+  //void writeLmpDataDebug(Config&, string);
+  void writeCfgData(const Config&, string);
+  void writeCfgAux(const Config&, const vector<int>&, string);
+
+  void writePOSCARVis(Config&, string, string);
+  map<string, int> writePOSCAR(Config&, string);
+
+  /* gbInCnf.cpp */
+  void cnvVec2Mat(const vector<double>&, Config&);
+  void cnvMat2Vec(Config&);
+  vector<double> cnvVecXY2VecAng(const vector<double>&);
+
+  /* neighbor.cpp*/
+  int getExpdParam(const Config&, const double);
+  vector<KNAtom> expandCellZ(const Config&, const int);
+  double calDist(const vector<double>, const KNAtom&, const KNAtom&);
+  double calDistPrl(const vector<double>&, const KNAtom&, const KNAtom&);
+  void getNBL(Config&, double);
+  void getNBL_serial(Config&, double);
+
+  /* gbBox.cpp */
+  void initBox(Config&);
+  void wrapAtomPos(Config&);
+  void wrapAtomPrl(Config&);
+  void cnvprl2pst(Config&);
+  void cnvpst2prl(Config&);
+  void perturb(Config&);
+
+  /* FCCConfig.cpp */
+  Config getFCCConv(const double, const string, const vector<int>&);
+
+  /* KNSolidSol.cpp
+   * elems stores name of elements
+   * nums stores their corresponding numbers
+   */
+  void getRandConf(Config&, \
+                   std::default_random_engine&, \
+                   const vector<string>&, \
+                   const vector<int>&);
+
+  void getRandConfUniformDist(Config&, \
+                              std::default_random_engine&, \
+                              vector<string>&, \
+                              const vector<int>&);
+
+  vector<pair<int, int>> getPairToSwap(Config&);
+
+  Config swapPair(const Config&, pair<int, int>);
+
+  /* KNEncodeCart.cpp */
+  vector<vector<string>> encodeConfig(Config&, \
+                                      const vector<int>&, \
+                                      const double, \
+                                      vector<string>&, \
+                                      const vector<int>&, \
+                                      const bool);
+
+  vector<pair<int, int>> readPairs(string);
+  Config rotateJumpPair(Config&, const vector<int>, const Config&);
+  Config rotateConfig(Config&, const vector<double>&);
+
+  vec getCenterShift(Config&);
+  void shiftToCenter(Config&, vector<double>&);
+  mat getJumpCoor(const Config&, const vector<int>&, const Config&);
+
+  /* KNBondCount.cpp */
+  map<string, int> countPairs(Config&, \
+                              const vector<string>&, \
+                              const vector<int>& );
+
+  /* KMCSimulation.cpp */
+  vector<double> calBarrierAndEdiff(Config&, \
+                                    const double&, \
+                                    const double&, \
+                                    const string&, \
+                                    unordered_map<string, double>&, \
+                                    Model&, \
+                                    Model&, \
+                                    const pair<int, int>&, \
+                                    const bool&, \
+                                    vector<string>&, \
+                                    const vector<double>&);
+
+  vector<double> calBarrierAndEdiff_LRU(Config&, \
+                                        const double&, \
+                                        const double&, \
+                                        const string&, \
+                                        unordered_map<string, double>&, \
+                                        Model&, \
+                                        Model&, \
+                                        const pair<int, int>&, \
+                                        const bool&, \
+                                        vector<string>&, \
+                                        const vector<double>&, \
+                                        LRUCache*);
+
+
+  double offsetBarrier(const Config&, \
+                       vector<string>&, \
+                       const vector<double>&, \
+                       const pair<int, int>&);
+
+  /* findClusters.cpp */
+
+  // This function give a set which contains all the solute atoms ID
+  unordered_set<int> findSoluteAtoms(const Config&, const string&);
+
+  // This is a helper function which is to find X clusters after
+  // removing a certain element and returns X.
+  int helperBFS(const Config&, \
+                const unordered_set<int>&, \
+                unordered_multimap<int, int>&, \
+                map<int, int>&);
+
+  void getLargestClts(const int&, \
+                      const int&, \
+                      unordered_multimap<int, int>&, \
+                      map<int, int>&);
+
+  void helperAddFNNs(const Config&, \
+                     unordered_multimap<int, int>&, \
+                     map<int, int>&);
+  // This function returns X largest clusters with FNBs
+  map<int, int> findAtm2Clts(Config&, const int&, const string&);
+
+  /* KNOrdered.cpp */
+  Config embedCluster(const Config&, \
+                      const pair<string, string>&, \
+                      const ODS::OrderedStruct&, \
+                      const int&);
 };
-// } // end namespace LR
+
+#include "Elem.inl"
 #endif
